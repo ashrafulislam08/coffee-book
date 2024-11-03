e05c40f20f427c0fe24a2a4cb29df3af9358bc00
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-03 11:29:46 +0600

    Heading styles completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Heading from "../components/Heading";
 
 const Home = () => {
   return (
     <div>
       {/* Banner */}
       <Banner />
+      {/* Heading */}
+      <Heading
+        title={"Browse Coffees by Category"}
+        subtitle={
+          "Choose your desired coffee category to browse through specific coffees that fit in your taste."
+        }
+      />
     </div>
   );
 };
